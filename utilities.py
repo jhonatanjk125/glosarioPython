409ec9c91b15dbc18f321f97faa7b0971d296e66
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-02-14 18:34:02 -0500

    feat: :sparkles: Add function to save data from a list to a JSON file

--- utilities.py
+++ utilities.py
@@ -17,3 +17,18 @@ def load_data_from_file(json_filename):
             return data_list
         except:
             return data_list
+
+
+def save_data_to_file(json_filename, data_list=[]):
+    """
+    Saves data to a JSON file.
+
+    Parameters:
+    - json_filename (str): The name of the JSON file to save to (excluding the ".json" extension).
+    - data_list (list): The data to be saved to the file. Defaults to an empty list if not provided.
+
+    Returns:
+    - None
+    """
+    with open(f"data/{json_filename}.json", "w", encoding="utf-8") as f:
+        json.dump(data_list, f, ensure_ascii=False, indent=4)
